d6cb11edd42a1a765e016f5e1a81aadb06756de9
Olivier Bossel <olivier.bossel@gmail.com>
2026-03-02 13:01:13 +0100

    chore: baseline before comprehensive reorganization - currency example completed

---
+++ src/shared/array/findAll.ts
@@ -0,0 +1,55 @@
+/**
+ * @name            findAll
+ * @namespace       shared.array
+ * @type            Function
+ * @platform        node
+ * @platform        js
+ * @status          stable
+ *
+ * This function is a subset of the native "find" but it returns all the items that match the check function.
+ * It returns an array of objects, each containing the:
+ * - index: the position in the array
+ * - value: the actual value at this position
+ *
+ * @param       {Array}         ar                  The array in which to search for an item
+ * @param       {Function}      check               The check function to test if the element is the searched one
+ * @return      {TFindAllResult[]|null}                An array of objects containing the index and value of the found items, or null if nothing found
+ *
+ * @snippet         findAll($1, (item: any) => {
+ *    $2
+ * })
+ *
+ * @example         js
+ * import { findAll } from '@blackbyte/sugar/array';
+ * findAll(['hello','world'], (item: any) => item === 'world')
+ * [
+ *     {
+ *         index: 1,
+ *         value: 'world'
+ *     }
+ * ]
+ *
+ * @since           1.0.0
+ * @author    Olivier Bossel <olivier.bossel@gmail.com> (https://blackbyte.space)
+ */
+
+export type TFindAllResult = {
+  index: number;
+  value: any;
+};
+
+export default function findAll(
+  ar: any[],
+  check: (item: any) => boolean,
+): TFindAllResult[] {
+  const results: TFindAllResult[] = [];
+  for (let item of ar) {
+    if (check(item)) {
+      results.push({
+        index: ar.indexOf(item),
+        value: item,
+      });
+    }
+  }
+  return results;
+}
